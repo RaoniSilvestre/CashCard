c40267be01d87e5f78485acfd624012a09a148d4
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-05-27 12:12:28 -0300

    first Post method

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -2,6 +2,8 @@ package example.cashcard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,8 +39,27 @@ class CashCardApplicationTests {
     ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-
     assertThat(response.getBody()).isBlank();
 
   }
+
+  @Test
+  void shouldCreateANewCard() {
+    CashCard newCashCard = new CashCard(null, 700.00);
+
+    ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+    assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+    URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+    ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+    assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+    DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+    Number id = documentContext.read("$.id");
+    Double amount = documentContext.read("$.amount");
+
+    assertThat(id).isNotNull();
+    assertThat(amount).isEqualTo(700.00);
+  }
+
 }
